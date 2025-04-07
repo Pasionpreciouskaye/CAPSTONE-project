a690101c2baa74d917d8c6e4f8a0de25b317c513
morcoso-daisy <morcosodaisy@gmail.com>
2025-04-07 14:50:51 +0800

    add changes

--- js/index.js
+++ js/index.js
@@ -62,35 +62,3 @@ carouselWrapper.addEventListener("scroll", () => {
 });
 
 updateArrows();
-
-const hamburgerIcon = document.getElementById("hamburger-icon");
-const sidebar = document.getElementById("sidebar");
-const overlay = document.querySelector(".overlay");
-const body = document.body;
-
-hamburgerIcon.addEventListener("click", () => {
-  sidebar.classList.toggle("active");
-  overlay.classList.toggle("active");
-  body.classList.toggle("no-scroll");
-});
-
-overlay.addEventListener("click", () => {
-  sidebar.classList.remove("active");
-  overlay.classList.remove("active");
-  body.classList.remove("no-scroll");
-});
-
-document.querySelectorAll(".sidebar ul li a").forEach((link) => {
-  link.addEventListener("click", () => {
-    sidebar.classList.remove("active");
-    overlay.classList.remove("active");
-    body.classList.remove("no-scroll");
-    window.location.href = "index.html";
-  });
-});
-
-sidebar.addEventListener("mouseleave", () => {
-  sidebar.classList.remove("active");
-  overlay.classList.remove("active");
-  body.classList.remove("no-scroll");
-});
