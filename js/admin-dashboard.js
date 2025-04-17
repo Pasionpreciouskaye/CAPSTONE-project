2066fd9cd473aa95669c481c11b57fe3767c40df
Pasionpreciouskaye <kayeepasion@gmail.com>
2025-04-18 01:09:02 +0800

    deleting some line for js ang fixing connections

--- js/admin-dashboard.js
+++ js/admin-dashboard.js
@@ -1,3 +1,8 @@
+const pb = new PocketBase("http://127.0.0.1:8090");
+if (!pb.authStore.isValid) {
+    location.href="landing.html"
+} 
+
 document.addEventListener('DOMContentLoaded', () => {
     const sampleUserData = {
         name: "Mark Admin",
